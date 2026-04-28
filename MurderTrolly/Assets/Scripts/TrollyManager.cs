194b906db22c233f858b2fc91d949fa3368d8af7
Kai <Kaiboucher2007@gmail.com>
2026-04-28 03:49:32 +0100

    Add TrollyManager and update Level 1 scene
    Add new TrollyManager script (and .meta) and make large edits to Level 1.unity: add path waypoints and track object groups (P1/P2/PathStart/Track Objects), multiple cube props with colliders and mesh/renderer updates, a UI Button, and reorganize/adjust transforms, colliders, materials and component references (including changes to a Head/Global Volume object). Update ProjectSettings/TagManager to add path tags and tweak scene hierarchy. Minor README.md changes included.

---
+++ MurderTrolly/Assets/Scripts/TrollyManager.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class TrollyManager : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public int pathChoice = 1;
+
+    private bool reachedMiddle = false;
+    private bool reachedPathMiddle = false;
+    private bool reachedPathEnd = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform middlePos;
+    [SerializeField] private Transform middle1Pos;
+    [SerializeField] private Transform middle2Pos;
+    [SerializeField] private Transform end1Pos;
+    [SerializeField] private Transform end2Pos;
+
+    [SerializeField] private Camera povCamera;
+    [SerializeField] private Camera topDownCamera;
+
+    void Start()
+    {
+        povCamera.gameObject.SetActive(false);
+        topDownCamera.gameObject.SetActive(true);
+    }
+    
+    public void SetPath(int choice)
+    {
+        this.pathChoice = choice;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step =  speed * Time.deltaTime; // calculate distance to move
+        if (!reachedMiddle)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, middlePos.position, step);
+            if (transform.position == middlePos.position)
+            {
+                reachedMiddle = true;
+            }
+        } else if (!reachedPathMiddle)
+        {
+            povCamera.gameObject.SetActive(true);
+            topDownCamera.gameObject.SetActive(false);
+            if (pathChoice == 1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, middle1Pos.position, step);
+                if (transform.position == middle1Pos.position)
+                {
+                    reachedPathMiddle = true;
+                }
+            } else if (pathChoice == 2)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, middle2Pos.position, step);
+                if (transform.position == middle2Pos.position)
+                {
+                    reachedPathMiddle = true;
+                }
+            }
+        } else if (!reachedPathEnd)
+        {
+            if (pathChoice == 1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, end1Pos.position, step);
+                if (transform.position == end1Pos.position)
+                {
+                    reachedPathEnd = true;
+                }
+            } else if (pathChoice == 2)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, end2Pos.position, step);
+                if (transform.position == end2Pos.position)
+                {
+                    reachedPathEnd = true;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -22,9 +22,11 @@ The person thats setting these up is using more and more alaberate was to make i
 | make some tracks for the trolly to go on                  | 1h            | 0.5h        | 1               | 1             | 01:01 28/04 | 01:32 28/04   |
 | make a simple platform for the camera to go on the tracks | 5h            | 0.5h        | 3               | 2             | 01:32 28/04 | 02:04 28/04   |
 | Simple Demo UI                                            | 1h            | 0.5h        | 1               | 5             | 02:04 28/04 | 02:36 28/04   |
-| make the platform move forwards                           | 5h            |             | 5               |               |             |               |
-| registor player input for track 1 or 2                    | 8h            |             | 8               |               |             |               |
+| make the platform move forwards                           | 5h            | 0.75h       | 5               | 2             | 02:36 28/04 | 03:17 28/04   |
+| ~~registor player input for track 1 or 2~~                | ~~8h~~        |             | ~~8~~           |               |             |               |
+| Make the UI buttons set the path in the trolly manager    | 1h            | 0.25h       | 2               | 1             | 03:21 28/04 | 03:35 28/04   |
 | make the platform move on the correct track               | 10h           |             | 9               |               |             |               |
 
 Plan changes
 1. The camera only goes to the driver POV onces the player has locked in there choice other wise its top down
+2. using UI buttons rather than keyboard inputs for simplcity
